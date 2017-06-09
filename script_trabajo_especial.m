27a45dcf6fde9d96d4589cde9a53d7b1a86368a8
garciareinhold <arturogreinhold@gmail.com>
2017-06-09 18:40:29 -0300

    Termine el informe

--- script_trabajo_especial.m
+++ script_trabajo_especial.m
@@ -3,9 +3,12 @@
 clear
 clc
 close all
+
 dni=37120008;
 
+%Epsilon de 0.1
 tic
+
 epsilon=0.1;
 [probabilidad, probabilidades_parciales] = calcular_probabilidad_realizar_disparo (dni, epsilon);
 fprintf('La probabilidad de que me autoricen realizar dos disparos seguidos con un epsilon=0.1 es: %f \n', probabilidad);
@@ -16,11 +19,14 @@ xlabel('Numero de iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
 legend('Evolucion de la probabilidad');
+
 tiempo= toc;
 fprintf('El tiempo de convergencia es: %f \n',tiempo);
 
 
+%Epsilon de 0.01
 tic
+
 epsilon2=0.01;
 [probabilidad, probabilidades_parciales] = calcular_probabilidad_realizar_disparo (dni, epsilon2);
 fprintf('La probabilidad de que me autoricen realizar dos disparos seguidos con un epsilon=0.01 es: %f \n', probabilidad);
@@ -31,10 +37,14 @@ xlabel('Numero de iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
 legend('Evolucion de la probabilidad');
+
 tiempo=toc;
 fprintf('El tiempo de convergencia es: %f \n',tiempo);
 
+
+%Epsilon de 0.001
 tic
+
 epsilon3=0.001;
 [probabilidad, probabilidades_parciales] = calcular_probabilidad_realizar_disparo (dni, epsilon3);
 fprintf('La probabilidad de que me autoricen realizar dos disparos seguidos con un epsilon=0.001 es: %d\n', probabilidad);
@@ -45,5 +55,6 @@ xlabel('Numero de iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
 legend('Evolucion de la probabilidad');
+
 tiempo=toc;
 fprintf('El tiempo de convergencia es: %f \n',tiempo);
